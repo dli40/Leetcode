9462d6848959b2b48326f9e947f8c738fcd8adbc
Daniel Li <dli40@calpoly.edu>
2018-09-11 01:39:39 -0700

    Create intersectionOfTwoArrays.py

---
+++ intersectionOfTwoArrays.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        
+        if len(nums1)<len(nums2):
+            smaller = nums1
+            other = nums2
+        else:
+            smaller = nums2
+            other = nums1
+        
+        for num in smaller:
+            if num in other and num not in result:
+                result.append(num)
+        return result
+        
+        
+        #time: O(n)
+        #space: O(n)
